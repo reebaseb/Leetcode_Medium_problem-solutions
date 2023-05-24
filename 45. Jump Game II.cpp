667bb9631fa9812c6e80fef6223f0e10ee908ab2
Reeba Sebastian <72300589+reebaseb@users.noreply.github.com>
2023-05-25 00:00:49 +0530

    Create 45. Jump Game II.cpp

---
+++ 45. Jump Game II.cpp
@@ -0,0 +1,21 @@
+// C++
+
+class Solution {
+public:
+    int minjump(int i,vector<int>& nums,vector<int>& dp)
+    {
+        if(i >= nums.size()-1){
+            return 0;
+        }
+        if (dp[i] != -1 ) return dp[i];
+        int mini= 1e9;
+        for(int k=i+1;k<=i+nums[i];k++){
+            mini = min(mini,1+minjump(k,nums,dp));
+        }
+        return dp[i]=mini;
+    }
+    int jump(vector<int>& nums) {
+        vector<int> dp(nums.size()+1,-1);
+        return minjump(0,nums,dp);
+    }
+};
